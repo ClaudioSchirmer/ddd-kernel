fc47e2af482027440be65d9f026c7794d0c9cccd
Claudio Schirmer Guedes <claudioschirmer@icloud.com>
2022-01-18 22:16:56 -0300

    Log
    + Remover log

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,13 +40,13 @@ java {
 }
 
 group = "dev.cschirmer"
-version = "3.1.0"
+version = "3.2.0"
 
 sourceSets.main {
 	withConvention(KotlinSourceSet::class) {
 		kotlin.srcDirs("src")
 	}
-	resources.srcDirs("resources")
+	//resources.srcDirs("resources")
 }
 
 sourceSets.test {
