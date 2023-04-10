6e9bba63f2d26cb86328f0e2453c5bcb3fc2b369
Cláudio Schirmer Guedes <claudio@Cludios-MacBook.local>
2023-04-10 14:15:44 -0300

    open context

--- src/br/dev/schirmer/ddd/kernel/application/configuration/AppContext.kt
+++ src/br/dev/schirmer/ddd/kernel/application/configuration/AppContext.kt
@@ -3,6 +3,6 @@ package br.dev.schirmer.ddd.kernel.application.configuration
 import br.dev.schirmer.ddd.kernel.domain.models.Context
 import java.util.*
 
-class AppContext(
+open class AppContext(
     override val id: UUID
 ) : Context
